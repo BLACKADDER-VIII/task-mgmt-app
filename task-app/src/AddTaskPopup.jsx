279f9eaf3a9f3656d097e94f0a2f5b9e51856966
BLACKADDER-VIII <dhroov.1a@gmail.com>
2026-04-11 14:28:56 -0500

    Brief overlay of home page

---
+++ task-app/src/AddTaskPopup.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useRef, useState } from "react";
+import './styles/AddTaskPopup.css';
+
+
+export default function AddTaskPopup({ taskArray, setTaskArray, closePopup, setClosePopup }){
+    if (closePopup==1)
+        return null;
+    
+    const [title, setTitle] = useState('');
+    const [priority, setPriority] = useState('high');
+    const [dueDate, setDueDate] = useState('');
+
+    const reset = ()=>{
+        setTitle('');
+        setPriority('');
+        setDueDate('');
+    }
+
+    const addTaskHandler = ()=>{
+        // Add conditions to check valid task
+        if (!title || !priority || !dueDate){
+            alert("Please fill all fields");
+            return;
+        }
+        const curr_task = {
+            title: title,
+            priority: priority,
+            dueDate: dueDate
+        }
+        setTaskArray([...taskArray, curr_task]);
+        reset();
+        setClosePopup(1);
+    }
+
+    const closeHandler = ()=>{
+        reset();
+        setClosePopup(1);
+    }
+
+    return (
+        <div className='add-task-popup'>
+            <div className="close-btn-div">
+                <button onClick={closeHandler} className="close-btn">X</button>
+            </div>
+            <div className="fill-info-grid">
+                <label>Task</label> <input placeholder="Enter task" onChange={(e)=>setTitle(e.target.value)}/>
+                <label>Priority</label> <select className="priority-selector" onChange={(e)=>{setPriority(e.target.value)}}>
+                        <option value='high'>High</option>
+                        <option value='medium'>Medium</option>
+                        <option value='low'>Low</option>
+                    </select> 
+                <label>Due Date</label> <input type='date' onChange={(e)=>setDueDate(e.target.value)}/>
+            </div>
+            <div className="add-btn-div">
+                <button onClick={addTaskHandler} className="add-btn">Add</button>
+            </div>  
+        </div>
+    );
+}
